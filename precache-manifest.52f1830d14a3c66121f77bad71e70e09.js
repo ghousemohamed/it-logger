c7fa8f76b399d192831af20de7e87d55799203da
ghousemohamed <ghousemohamed7126@gmail.com>
2019-12-22 18:24:19 +0530

    Updates

---
+++ precache-manifest.52f1830d14a3c66121f77bad71e70e09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23baa48a3dd14d766fdda55ed99cf5a9",
+    "url": "/it-logger/index.html"
+  },
+  {
+    "revision": "46448ac50a9e9b8f2ba3",
+    "url": "/it-logger/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "b6eb15ad37571ee444b0",
+    "url": "/it-logger/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "46448ac50a9e9b8f2ba3",
+    "url": "/it-logger/static/js/2.fadd1756.chunk.js"
+  },
+  {
+    "revision": "b6eb15ad37571ee444b0",
+    "url": "/it-logger/static/js/main.ee0a1f9b.chunk.js"
+  },
+  {
+    "revision": "8b62bac2b9be40a14b17",
+    "url": "/it-logger/static/js/runtime~main.b2458625.js"
+  }
+]);
